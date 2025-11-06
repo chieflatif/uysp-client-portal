e201f21a9ecc3ec289ffbe6030f2296d340a690c
Chieflatif <latifhorst@gmail.com>
2025-11-05 21:20:02 -0800

    Fix JSX syntax error in CustomCampaignForm variable display

--- src/components/admin/CustomCampaignForm.tsx
+++ src/components/admin/CustomCampaignForm.tsx
@@ -1253,14 +1253,14 @@ export default function CustomCampaignForm({
                       <p className="text-orange-400 text-xs mt-1">{errors[`message_${index}_paste`]}</p>
                     )}
                     <p className="text-xs text-gray-500 mt-1">
-                      Variables: <code className="text-cyan-300">{{'{{'}}first_name{{'}}'}}</code>, 
+                      Variables: <code className="text-cyan-300">{'{{first_name}}'}</code>,
                       {resourceLink && resourceName && (
                         <>
-                          <code className="text-purple-300 ml-1">{{'{{'}}resource_name{{'}}'}}</code>, 
-                          <code className="text-purple-300 ml-1">{{'{{'}}resource_link{{'}}'}}</code>, 
+                          <code className="text-purple-300 ml-1">{'{{resource_name}}'}</code>,
+                          <code className="text-purple-300 ml-1">{'{{resource_link}}'}</code>,
                         </>
                       )}
-                      <code className="text-green-300 ml-1">{{'{{'}}booking_link{{'}}'}}</code>
+                      <code className="text-green-300 ml-1">{'{{booking_link}}'}</code>
                     </p>
                   </div>
                 </div>
